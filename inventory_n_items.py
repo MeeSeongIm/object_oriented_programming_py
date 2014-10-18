29d72383dad54a906a74bf5247127582b837cf8b
Mee Seong Im <MeeSeongIm@users.noreply.github.com>
2014-10-18 14:23:30 -0500

    Update inventory_n_items.py

--- inventory_n_items.py
+++ inventory_n_items.py
@@ -21,12 +21,12 @@ while new_item in yes:
     stock.append(i)
     quantity_available_item = input("New Item %s Quantity: " % stock[i])
     if quantity_available_item.isnumeric() == True:
-        quantity_available[i] = quantity_available_item
+        quantity_available[i] = int(quantity_available_item)
     else:     
         quantity_available[i] = 0
     cost_item = input("New Item %s Cost: " % stock[i])
     if cost_item.isnumeric() == True:
-        cost[i] = cost_item
+        cost[i] = float(cost_item)
     else:
         cost[i] = 10                     
     new_item = input("New item? ").lower()
@@ -52,11 +52,11 @@ for i in range(len(stock)):
 for i in range(len(stock)):
     quantity_sold_item = input("Number of Item %s sold: " % stock[i])
     if quantity_sold_item.isnumeric() == True:
-        if quantity_sold_item > quantity_available[i]:
+        if int(quantity_sold_item) > quantity_available[i]:
             quantity_sold[i] = quantity_available[i]
             i += 1
         else:             
-            quantity_sold[i] = quantity_sold_item
+            quantity_sold[i] = int(quantity_sold_item)
             i += 1
     else:
         quantity_sold[i] = 0
@@ -74,12 +74,12 @@ file.write("Quantifying New Arrival. \n")
 for i in range(len(stock)):
     new_arrival_item = input("Number of Item %s shipped in: " % stock[i])
     if new_arrival_item.isnumeric() == True:
-        new_arrival[i] = new_arrival_item
+        new_arrival[i] = int(new_arrival_item)
     else:
         new_arrival[i] = 0
         
 for i in range(len(stock)):
-    quantity_left[i] = int(quantity_available[i]) - int(quantity_sold[i]) + int(new_arrival[i])
+    quantity_left[i] = quantity_available[i] - quantity_sold[i] + new_arrival[i]
 
 print("\n")
  
@@ -97,23 +97,24 @@ net_total = 0
 
  
 for i in range(len(stock)):
-    gross_total += int(quantity_sold[i])*int(cost[i])
+    gross_total += quantity_sold[i]*cost[i]
     
 
 for i in range(len(stock)):
-    net_total += int(quantity_sold[i])*int(cost[i])*0.10      # 10% per item sold is the net profit 
+    net_total += quantity_sold[i]*cost[i]*0.10      # 10% per item sold is the net profit 
 
 
 profit.append(net_total)
 
 print("Gross Total for the Day: $%s." % gross_total)
 print("Net Total for the Day: $%s." % net_total)
+file.write("\n")
 file.write("Gross Total for the Day: $%s. \n" % gross_total)
 file.write("Net Total for the Day: $%s. \n" % net_total)
 
 
 print("\n")  
-print("Summary of Total Inventory Per Day: %s " % day)   
+print("Summary of Total Inventory Per Day: %s " % day)    
 print("Net Total in Dollars: %s" % profit)  
 file.write("Summary of Total Inventory Per Day: %s \n" % day)
 file.write("Net Total in Dollars: %s \n" % profit)
