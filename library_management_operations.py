05bd5c92a531b381c09fd3230099e08ce82e9552
Mee Seong Im <MeeSeongIm@users.noreply.github.com>
2014-11-01 22:00:24 -0500

    Create library_management_operations.py

---
+++ library_management_operations.py
@@ -0,0 +1,68 @@
+
+# library system
+# library management operations: registering a student, issue or return a book, late fees, etc. 
+
+from datetime import datetime 
+
+months = 1
+late_fee_per_day = 0.15
+
+class Library_Account(object):
+    def __init__(self, first_name, last_name, student_id):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.student_id = student_id
+        self.student = None 
+
+    def card(self, card_no):
+        self.card_no = card_no
+        
+    def return_card_no(self):
+        return self.card_no             # prints card number
+
+    def status(self, faculty_staff, grad_student_employee):
+        self.faculty_staff = faculty_staff 
+        self.grad_student_employee = grad_student_employee           
+
+    def checkout(self, call_number):
+        self.call_number = call_number
+        print("%s %s: this book associated to the Call Number %s is due in %s month." % (self.first_name, self.last_name, self.call_number, months) )
+
+    def late_fee(self, days):
+        self.days = days
+        print("%s %s: your late fee is $%s per day. So far, your book is late by %s days." % (self.first_name, self.last_name, late_fee_per_day, self.days))
+        
+class Faculty_Staff_Grad(Library_Account):
+    def longer_checkout(self, call_number):
+        self.call_number = call_number
+        print("%s %s: this book associated to the Call Number %s is due in %s months." % (self.first_name, self.last_name, self.call_number, 6*months))
+
+    def late_fee(self, days):
+        self.days = days
+        print("%s %s: your late fee is $%s per day. So far, your book is late by %s days." % (self.first_name, self.last_name, 4*late_fee_per_day, self.days))
+
+
+#    def late_fee(self, days):                  
+#        super(Faculty_Staff_Grad, self).late_fee()
+
+new_student = Library_Account("Matthew", "Thompson", 898239289)
+new_student.card("0923402394")
+print(new_student.return_card_no())             # prints card number 
+
+new_student.status(False, False)
+print(new_student.faculty_staff)
+print(new_student.grad_student_employee)
+
+new_student.checkout("510_Am4p_v.29")
+new_student.late_fee(0)
+
+new_faculty = Faculty_Staff_Grad("Mary", "Jones", "012984378")
+new_faculty.card("2009834598")
+new_faculty.longer_checkout("516.35_N145l")
+print(new_faculty.return_card_no())
+new_faculty.late_fee(3)
+
+
+
+
+
